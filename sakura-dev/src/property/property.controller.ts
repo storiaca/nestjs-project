57841c1e9a1616a99bfbf7797f34d8e66e21a390
Aleksandar Ristic <storiaca@gmail.com>
2024-10-17 14:00:35 +0200

    Dynaimc routes

--- sakura-dev/src/property/property.controller.ts
+++ sakura-dev/src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -7,6 +7,15 @@ export class PropertyController {
     return 'All Properties';
   }
 
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return id;
+  // }
+
+  @Get(':id/:slug')
+  findOne(@Param('id') id, @Param('slug') slug) {
+    return `id = ${id}, slug = ${slug}`;
+  }
   @Post()
   craete() {
     return 'This will create a property';
